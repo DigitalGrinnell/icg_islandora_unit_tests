76b96dec96e98c1f396811812800db3a07ab0809
McFateM <mcfatem@grinnell.edu>
2018-01-02 08:24:52 -0600

    More DG specifics

--- isle-dev.cfg.php
+++ isle-dev.cfg.php
@@ -5,19 +5,19 @@
 	# ---------------------------
 
 	# Set these configuration settings!
-	define('APP_PREFIX', 'https://'); // choose either: 'http://' OR 'https://'
-	define('APP_DOMAIN', 'digital.grinnell.edu'); // exclude prefix (http:// or https://); exclude trailing slash
+	define('APP_PREFIX', 'http://'); // choose either: 'http://' OR 'https://'
+	define('APP_DOMAIN', 'isle-dev.grinnell.edu'); // exclude prefix (http:// or https://); exclude trailing slash
 	define('APP_PORT', ''); // optional: port value (i.e. ":8888")
 	define('APP_FOLDER', ''); // optional: subdirectory path (i.e. "/islandora"); exclude trailing slash
 
-	define('FEDORA_PREFIX', 'https://'); // choose either: 'http://' OR 'https://'
-	define('FEDORA_DOMAIN', 'digital.grinnell.edu'); // exclude prefix (http:// or https://); exclude trailing slash
+	define('FEDORA_PREFIX', 'http://'); // choose either: 'http://' OR 'https://'
+	define('FEDORA_DOMAIN', 'isle-dev.grinnell.edu'); // exclude prefix (http:// or https://); exclude trailing slash
 	define('FEDORA_PORT', ':8080'); // optional: port value (i.e. ":8888")
 	define('FEDORA_FOLDER', '/fedora'); // optional: subdirectory path (i.e. "/fedora"); exclude trailing slash
 
 	# Test User credentials
-	define('APP_TEST_USER', 'System Admin');
-	define('APP_TEST_PASS', 'EnterPasswordHere');
+	define('APP_TEST_USER', 'islandora_docker_admin');
+	define('APP_TEST_PASS', 'islandoradockeradminpw2017');
 
 
 	#############################################################
